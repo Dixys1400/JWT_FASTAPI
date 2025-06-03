791050b58a62e1d0d16f5bc76311d9961bcb2dce
Dixys1400 <dimamaper@gmail.com>
2025-06-03 19:07:35 +0200

    Добавил новый роут

--- main.py
+++ main.py
@@ -36,15 +36,18 @@ def sign_up(request: SignUpSchema, db: Session = Depends(get_db)):
     return {"access_token": sign(user.email), "token_type": "bearer"}
 
 
-@app.post("signin")
+@app.post("/signin")
 def sign_in(request: SignInSchema, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == request.email).first()
-    if not user or not verify_password(request.password, user.jhashed_password):
+    if not user or not verify_password(request.password, user.hashed_password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
     return {"access_token": sign(user.email), "token_type": "bearer"}
 
 
-
+@app.get("me")
+def get_me(decoded: dict = Depends(decode), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == decoded["email"]).first()
+    return {"name": user.name, "email": user.email}
 
 
 @app.post("/authtest")
